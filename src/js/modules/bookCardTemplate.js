20dd69e499b24536055e598b8399500736d4c7cb
Musalaeva Larisa <musssss.94@mail.ru>
2019-01-06 19:27:28 +0300

    Правки в стилях для табов

--- src/js/modules/bookCardTemplate.js
+++ src/js/modules/bookCardTemplate.js
@@ -1,42 +1,20 @@
-// const bookCardTemplate = {
-
-//   wrap: '.j-product-cards',
-//   tag: 'a',
-//   tagClass: 'product-card-mini-wrap',
-//   setContent: function(data) {
-//     return `<a class="product-card-mini-wrap" href="">
-//     <article class="product-card-mini">
-//     <h2 class="product-card-mini__title">${data.name}</h2>
-//     <a class="product-card-mini__img-wrap">
-//     <img class="product-card-mini__img" src="img/${data.uri}.png" alt="${data.name}">
-//     </a>
-//     <p class="product-card-mini__descr">${data.desc}</p>
-//     <div class="product-card-mini__price">${data.price}</div>
-//     </article>
-//     </a>`
-//   }
-// };
-
-// export default bookCardTemplate;
-
 
 const bookCardTemplate = {
 
   wrap: '.j-product-cards',
-  tag: 'article',
+  tag: 'a',
   tagClass: 'product-card-mini',
   setContent: function(data) {
-    return `<article class="product-card-mini">
-    <a class="product-card-mini__inner" href="#">
+    return `<a class="product-card-mini" href="${data.uri}">
     <h2 class="product-card-mini__title">${data.name}</h2>
     <div class="product-card-mini__img-wrap">
-    <img class="product-card-mini__img" src="img/book-ne-ochevidno.jpg" alt="${data.name}">
+    <img class="product-card-mini__img" src="img/${data.uri}.jpg" alt="${data.name}">
     </div>
     <p class="product-card-mini__descr">${data.desc}</p>
-    </a>
     <span class="product-card-mini__price">${data.price}</span>
-    </article>`
+    </a>`
   }
 };
 
 export default bookCardTemplate;
+
